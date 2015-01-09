23b86bc0ad3cc1d3b5c3e339c1d6bdad2c1965fb
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-09 16:52:16 -0500

    basic firstworld

---
+++ src/tankattack/FirstWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tankattack;
+
+import javafx.stage.*;
+
+/**
+ *
+ * @author Ruslan
+ */
+public class FirstWorld extends World {
+
+    FirstWorld(Stage primaryStage) {
+
+        super(primaryStage);
+        
+    }
+    
+    
+    
+    
+    
+}
