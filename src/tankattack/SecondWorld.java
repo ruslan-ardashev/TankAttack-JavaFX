7d558c50e8142c252baaf96d0a3ed96b93db002a
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-10 17:30:01 -0500

    starts implementing a second world

---
+++ src/tankattack/SecondWorld.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tankattack;
+
+import Sprites.*;
+import javafx.stage.*;
+
+/**
+ *
+ * @author Ruslan
+ */
+public class SecondWorld extends World {
+
+    SecondWorld(Stage stage) {
+
+        super(stage);
+    
+    }
+
+    @Override
+    public void createInitialSprites() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void signalEndOfLevel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
