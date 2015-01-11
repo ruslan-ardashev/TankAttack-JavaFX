b2c556fe1e833f54fb34ee0e793026783957e19d
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-11 14:24:14 -0500

    creates three minions with revised constructors! movement getting there

--- src/tankattack/FirstWorld.java
+++ src/tankattack/FirstWorld.java
@@ -45,17 +45,19 @@ public class FirstWorld extends World {
     }
 
     private void createEnemies() {
-
-        // Enemy 1
-        Minion one = new Minion(50, 40.0, this);
+        
+        // Minion(double x, double y, World world, double leftXLimit, double rightXLimit, boolean goingRight)
+        
+//         Enemy 1
+        Minion one = new Minion(50.0, 40.0, this, 30.0, 150.0, true);
         
         
         // Enemy 2
-        Minion two = new Minion(245.0, 80.0, this);
+        Minion two = new Minion(245.0, 80.0, this, 150.0, 400.0, false);
         
         
         // Enemy 3
-        Minion three = new Minion(540.0, 60.0, this);
+        Minion three = new Minion(540.0, 60.0, this, 400.0, 560.0, true);
         
         
         
