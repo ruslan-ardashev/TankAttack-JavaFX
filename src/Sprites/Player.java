e7427b13e0f7078576eddf106c0d0a49c995db8f
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-11 13:01:54 -0500

    revises player constructor to include image

--- src/Sprites/Player.java
+++ src/Sprites/Player.java
@@ -5,10 +5,30 @@
  */
 package Sprites;
 
+import javafx.scene.image.*;
+import tankattack.*;
+
 /**
  *
  * @author Ruslan
  */
 public class Player extends Sprite {
+
+    
+    public Player() {
+        
+        super();
+        this.setImage(new Image(getClass().getResourceAsStream("testTank.png")));
+        
+        this.setWidth(this.getImage().getWidth());
+        this.setHeight(this.getImage().getHeight());
+        
+        this.setTranslateX((TankAttack.gameWidth  - width())/2);
+        this.setTranslateY((TankAttack.gameHeight - height())/2);
+        
+        this.healthBar = null;
+        System.out.println("null healthbar for now for player in constructor");
+                
+    } 
     
 }
